aea27c307c2d6591c5b7dced30a0f87787086079
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-02-19 22:27:13 +0700

    feat: implement ContactComponent with modal for user inquiries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,11 @@
 
 import {
   Tooltip,
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
   Button,
   useDisclosure,
-  Form,
-  Input,
-  Textarea
 } from "@heroui/react";
 import Tabs from "@/components/Tabs";
+import ContactComponent from "@/components/ContactComponent";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
@@ -178,51 +171,7 @@ const Page = () => {
         <Tabs des={dataDeskriptions} educations={dataEducations} experiences={dataExperiences} resumeUrl={dataAboutMe.resumeUrl} />
       </div>
 
-      <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Contact Me</ModalHeader>
-              <Form className="w-full">
-                <ModalBody className="w-full">
-                  <Input
-                    isRequired
-                    errorMessage="Please enter a valid fullname"
-                    label="Full Name"
-                    labelPlacement="outside"
-                    name="fullname"
-                    placeholder="Enter your full name"
-                    type="text"
-                  />
-                  <Input
-                    isRequired
-                    errorMessage="Please enter a valid email"
-                    label="Email"
-                    labelPlacement="outside"
-                    name="email"
-                    placeholder="Enter your email"
-                    type="email"
-                  />
-                  <Textarea
-                    isRequired
-                    label="Description"
-                    labelPlacement="outside"
-                    placeholder="Enter your description"
-                  />
-                </ModalBody>
-                <ModalFooter className="flex w-full">
-                  <Button className="text-pink-500" variant="light" onPress={onClose}>
-                    Close
-                  </Button>
-                  <Button className="bg-pink-500 text-white" onPress={onClose}>
-                    Send
-                  </Button>
-                </ModalFooter>
-              </Form>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      <ContactComponent isOpen={isOpen} onOpenChange={onOpenChange} />
     </>
   )
 }
--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -1,11 +1,68 @@
 import React from 'react'
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input,
+  Textarea,
+  Form
+} from "@heroui/react";
 
-const ContactComponent = () => {
+interface ContactComponentProps {
+  isOpen: boolean;
+  onOpenChange: () => void;
+}
+
+const ContactComponent: React.FC<ContactComponentProps> = ({ isOpen, onOpenChange }) => {
   return (
-    <div>
-      <form action="" method="post">
-      </form>
-    </div>
+    <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">Contact Me</ModalHeader>
+            <Form className="w-full">
+              <ModalBody className="w-full">
+                <Input
+                  isRequired
+                  errorMessage="Please enter a valid fullname"
+                  label="Full Name"
+                  labelPlacement="outside"
+                  name="fullname"
+                  placeholder="Enter your full name"
+                  type="text"
+                />
+                <Input
+                  isRequired
+                  errorMessage="Please enter a valid email"
+                  label="Email"
+                  labelPlacement="outside"
+                  name="email"
+                  placeholder="Enter your email"
+                  type="email"
+                />
+                <Textarea
+                  isRequired
+                  label="Description"
+                  labelPlacement="outside"
+                  placeholder="Enter your description"
+                />
+              </ModalBody>
+              <ModalFooter className="flex w-full">
+                <Button className="text-pink-500" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button className="bg-pink-500 text-white" onPress={onClose}>
+                  Send
+                </Button>
+              </ModalFooter>
+            </Form>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   )
 }
 
