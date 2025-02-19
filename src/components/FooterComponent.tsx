087f8f561f44e824a648160d1b72ab7f1d53c1af
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-02-19 19:16:05 +0700

    fix: update FooterComponent background color for improved visibility

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -5,7 +5,7 @@ const FooterComponent = () => {
   const dateTime = now.getFullYear()
   console.log(dateTime);
   return (
-    <footer className='dark:bg-gray-200 p-20 flex flex-col items-center mb-16 sm:mb-0'>
+    <footer className='bg-gray-200 p-20 flex flex-col items-center mb-16 sm:mb-0'>
       <h1 className='text-6xl font-bold'>ARIALOG.</h1>
       <p>&copy; {dateTime} Nur Aria Hibnastiar. All Rights Reserved.</p>
     </footer>
