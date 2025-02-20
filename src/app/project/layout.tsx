463a56bb3f285aa14a2e46641d94cdc631e953d8
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-02-20 08:03:06 +0700

    feat: add Project and Showcase layout components with breadcrumbs and image display

---
+++ src/app/project/layout.tsx
@@ -0,0 +1,14 @@
+export default function ShowcaseLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <section className='header p-8 sm:mt-16 sm:py-16 bg-gray-100'>
+        <h1 className='text-5xl sm:text-8xl lg:text-9xl text-center capitalize font-bold'>PROJECT</h1>
+      </section>
+      <section>{children}</section>
+    </>
+  )
+}
---
+++ src/app/project/page.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+// import CardProject from '@/components/CardProject'
+import BentoCardComponent from '@/components/BentoCardComponent'
+
+const Project = () => {
+  // const data = [
+  //   {
+  //     'id': 1,
+  //     'url_img': '/img/profile.jpg',
+  //     'nama': 'Personal Website Portfolio 2025',
+  //     'kategori': "website",
+  //     'tahun': 2024,
+  //     'link_github': 'https://github.com/hibnastiar27',
+  //     'link_demo': 'https://skripsi.arialog.my.id',
+  //     'tech_stack': [
+  //       'javascript', 'next-js', 'tailwindcss', 'mongodb'
+  //     ]
+  //   },
+  //   {
+  //     'id': 2,
+  //     'url_img': '/img/profile.jpg',
+  //     'nama': 'Personal Website',
+  //     'kategori': "website",
+  //     'tahun': 2024,
+  //     'link_github': 'https://github.com/hibnastiar27',
+  //     'link_demo': 'https://skripsi.arialog.my.id',
+  //     'tech_stack': [
+  //       'javascript', 'next-js', 'tailwindcss', 'mongodb'
+  //     ]
+  //   },
+  //   {
+  //     'id': 3,
+  //     'url_img': '/img/profile.jpg',
+  //     'nama': 'Personal Website Portfolio 2025',
+  //     'kategori': "website",
+  //     'tahun': 2024,
+  //     'link_github': 'https://github.com/hibnastiar27',
+  //     'link_demo': 'https://skripsi.arialog.my.id',
+  //     'tech_stack': [
+  //       'javascript', 'next-js', 'tailwindcss', 'mongodb'
+  //     ]
+  //   },
+  //   {
+  //     'id': 4,
+  //     'url_img': '/img/profile.jpg',
+  //     'nama': 'Personal Website Portfolio 2025',
+  //     'kategori': "data",
+  //     'tahun': 2024,
+  //     'link_github': 'https://github.com/hibnastiar27',
+  //     'link_demo': 'https://skripsi.arialog.my.id',
+  //     'tech_stack': [
+  //       'javascript', 'next-js', 'tailwindcss', 'mongodb'
+  //     ]
+  //   },
+  //   {
+  //     'id': 5,
+  //     'url_img': '/img/profile.jpg',
+  //     'nama': 'Personal Website Portfolio 2025',
+  //     'kategori': "data",
+  //     'tahun': 2024,
+  //     'link_github': 'https://github.com/hibnastiar27',
+  //     'link_demo': 'https://skripsi.arialog.my.id',
+  //     'tech_stack': [
+  //       'javascript', 'next-js', 'tailwindcss', 'mongodb'
+  //     ]
+  //   },
+  // ]
+  return (
+    <>
+      {/* <section className='header p-8 sm:mt-16 sm:py-16 bg-gray-100'>
+        <h1 className='text-5xl sm:text-8xl lg:text-9xl text-center capitalize font-bold'>SHOWCASE</h1>
+      </section> */}
+      {/* <div className="w-full mb-16 mt-6 sm:mt-16 md:max-w-screen-xl md:mx-auto flex flex-col gap-6 px-6">
+        <div className="w-full">
+          <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-4 *:h-fit'>
+            {data.map((item, index) => {
+              return (
+                <CardProject
+                  key={index}
+                  judul={item.nama}
+                  tahun={item.tahun}
+                  github={item.link_github}
+                  demo={item.link_demo}
+                  tech={item.tech_stack}
+                  kategori={''}
+                  url_img={item.url_img}
+                />
+              )
+            })}
+          </div>
+        </div>
+      </div> */}
+
+      <div className="w-full mb-16 mt-6 sm:mt-16 md:max-w-screen-xl md:mx-auto flex flex-col gap-6 px-6">
+        <BentoCardComponent />
+      </div>
+    </>
+  )
+}
+
+export default Project
